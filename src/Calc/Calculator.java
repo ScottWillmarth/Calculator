86b7de405cc70d2e3a0bf936e939668f96800994
Scott Willmarth <scott.willmarth@hcl.com>
2021-01-29 12:03:46 -0500

    Implemented operator checker

--- src/Calc/Calculator.java
+++ src/Calc/Calculator.java
@@ -1,11 +1,46 @@
 package Calc;
+import java.util.Scanner;
+
+//Variables, data types, operators, type casting, control statements, 
+// class, objects, access specifiers, and core keywords like final, this, and static
 
 public class Calculator 
 {
+	public static void checkOperator(String operation)
+	{
+		if(!operation.equals("addition"))
+	    {
+	    	if(!operation.equals("subtraction"))
+		    {
+	    		if(!operation.equals("multiplication"))
+	    	    {
+	    			if(!operation.equals("division"))
+	    		    {
+	    				System.out.println("Please enter a valid operator, input is case sensitive");
+	    				@SuppressWarnings("resource")
+						Scanner myScan = new Scanner(System.in);
+	    				System.out.println("What operation will you be using?");
+	    			    System.out.println("addition, subtraction, multiplication, division");
+	    			    operation = myScan.next();
+	    			    checkOperator(operation);
+	    		    }
+	    	    }
+		    }
+	    }
+	}
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello, world!");
+		@SuppressWarnings("resource")
+		Scanner myScan = new Scanner(System.in);
+	    System.out.println("What operation will you be using?");
+	    System.out.println("addition, subtraction, multiplication, division");
+
+	    String operation = myScan.next();
+	    checkOperator(operation);
+	    
+	    System.out.println(operation + "= ");
+	    
 
 	}
 
